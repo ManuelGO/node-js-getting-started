e3c5da93a890293045ca8730d4e62b0439ead37d
Manuel Gonzalez <manuelarvelo@hotmail.com>
2020-12-17 12:25:04 +0100

    configurado bodyparser

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const app = express();
+const bodyParser = require("body-parser");
 
-express()
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+app.post("/token-device", (req, res)=> {
+  res.send(req.body.token);
+});
+
+app
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/android', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
